3ed032bc792e20556b76bd17982083c1fd4146f7
Davide Scriboni <davidescriboni94@gmail.com>
2022-05-14 18:04:33 +0200

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeria/Models/PizzaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LaMiaPizzeria.Models
+{
+    public class PizzaViewModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descrizione { get; set; }
+        public string? UrlImmagine { get; set; }
+        public double Prezzo { get; set; }
+    }
+}
